13eaed40e74c4057cd15c0a1e49a39b62a1dae13
Shristi Koju <kojushristi4@gmail.com>
2024-11-22 16:53:46 +0545

    squash commit merged
    squash title changed
    
    paragraph added

---
+++ src/components/squash.jsx
@@ -0,0 +1,11 @@
+const squash = () => {
+  return (
+    <div>
+      <h1>Squash Header</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem molestias quidem minus magni quos, voluptatem dolor voluptate cum error, delectus labore vero fugiat molestiae amet sit eaque ducimus qui corrupti?
+      Reiciendis expedita vel aliquid, a, distinctio ab impedit numquam sed ipsa saepe libero cupiditate esse vitae iusto illo repellat, recusandae consequuntur repudiandae! Vero illum velit explicabo fugit sit expedita? Ipsam!</p>
+    </div>
+  )
+}
+
+export default squash
